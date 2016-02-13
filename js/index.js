3a36d9ec0157e75c62fb734466e66681672b86c5
Boyoung Kwon <boyoung.kwon@gmail.com>
2016-02-12 16:25:04 -0800

    complete pomodoro clock v1

--- js/index.js
+++ js/index.js
@@ -1,90 +1,124 @@
-function setting(){
-  $('.setting').slideToggle();
-}
+//TODO
+//What happens if setting is changed while pomodoro clock is running?
+
+var pMin,
+    bMin,
+    totalSecs = 0,
+    totalBSecs = 0,
+    bmintosec,
+    minToSec,
+    handle;
+
 
 function swapBtns(){
   if($(event.currentTarget).attr('src') === 'img/player.png'){
     $(event.currentTarget).attr('src', 'img/stop.png');
-    pPlay();
+    Play();
   }else{
     $(event.currentTarget).attr('src', 'img/player.png');
-    pStop();
+    Stop();
   }
 }
 
-function pStop(){
-  location.reload();
+function Stop(){
+  var newPomodoro = $('.pmin').val();
+  var newBreak = $('.bmin').val();
+
+  $('.pmin').val(newPomodoro);
+  $('.bmin').val(newBreak);
+
+  $('.min').text(pad(newPomodoro));
+  $('.sec').text('00');
+  clearInterval(handle);
+
 }
 
-function pPlay(){
-  var pmin = Number($('.pmin').val());
-  var bmin = Number($('.bmin').val());
-  var totalSecs = 0;
-  var mintosec = pmin*60;
-  setInterval(pcountSecs, 1000);
+function Play() {
+  totalSecs = 0;
+  totalBSecs = 0;
+  pMin = Number($('.pmin').val());
+  bMin = Number($('.bmin').val());
+  minToSec = pMin * 60;
+  handle = setInterval(pcountSecs, 1000);
+}
 
-  function pcountSecs(){
-    totalSecs++;
-    if(totalSecs <= mintosec){
-      if(totalSecs === 60){
-        $('.sec').text(pad(Math.abs(totalSecs%60)));
-        $('.min').text(pad((pmin)-parseInt(totalSecs/60)));
-      }else{
-        $('.sec').text(pad(Math.abs(totalSecs%60-60)));
-        $('.min').text(pad((pmin-1)-parseInt(totalSecs/60)));
-      }
-    }else if(totalSecs === mintosec+1){
-      pBreak();
+function pcountSecs() {
+  totalSecs++;
+  if(totalSecs <= minToSec) {
+    if (totalSecs % 60 === 0) {
+      $('.sec').text(pad(Math.abs(totalSecs % 60)));
+      $('.min').text(pad((pMin) - parseInt(totalSecs / 60)));
+    } else {
+      $('.sec').text(pad(Math.abs(totalSecs % 60 - 60)));
+      $('.min').text(pad((pMin - 1) - parseInt(totalSecs / 60)));
     }
+  }else if (totalSecs === minToSec + 1) {
+    pBreak();
   }
+}
 
-  function pad(val){
-    var valString = val + "";
-    if(valString.length < 2){
-      return "0" + valString;
-    }
-    else
-    {
-      return valString;
-    }
+
+function pad(val){
+  var valString = val + "";
+  if(valString.length < 2){
+    return "0" + valString;
   }
-
-  function pBreak(){
-    playMusic();
-    $('#clock').css('border', '5px solid #00FF7F');
-    var totalBSecs = 0;
-    var bmintosec = (bmin)*60;
-    setInterval(bcountSecs, 1000);
-
-    function bcountSecs(){
-      totalBSecs++;
-      if(totalBSecs <= bmintosec){
-        if(totalBSecs === 60){
-          $('.sec').text(pad(Math.abs(totalBSecs%60)));
-          $('.min').text(pad((bmin)-parseInt(totalBSecs/60)));
-        }else{
-          $('.sec').text(pad(Math.abs(totalBSecs%60-60)));
-          $('.min').text(pad((bmin-1)-parseInt(totalBSecs/60)));
-        }
-      }else{
-        pStop();
-      }
-    }
-
-    function playMusic(){
-      var audio = new Audio("audio/beep.mp3");
-      audio.play();
-      return false;
-    }
+  else
+  {
+    return valString;
   }
+}
+
+function pBreak() {
+  playMusic();
+  $('#clock').css('border', '5px solid #4CA64C');
+  totalBSecs = 0;
+  bmintosec = bMin * 60;
+  setInterval(bcountSecs, 1000);
+}
+
+function bcountSecs(){
+  totalBSecs++;
+  if(totalBSecs <= bmintosec){
+    if(totalBSecs % 60 === 0){
+      $('.sec').text(pad(Math.abs(totalBSecs % 60)));
+      $('.min').text(pad((bMin) - parseInt(totalBSecs / 60)));
+    }else{
+      $('.sec').text(pad(Math.abs(totalBSecs % 60 - 60)));
+      $('.min').text(pad((bMin - 1) - parseInt(totalBSecs / 60)));
+    }
+  }else{
+    Stop();
+  }
+}
+
+function playMusic(){
+  var audio = new Audio("audio/beep.mp3");
+  audio.play();
+  return false;
 }
 
 $(document).ready(function(){
   $('.setting_img').click(function(){
-    setting();
-  })
+    $('.setting').slideToggle("fast", function(){
+      $('input[name="pomodoro"]').select();
+    });
+  });
+
+  $('#pomodoro_setup').on('submit', function(e){
+    e.preventDefault();
+    var newPomodoro = $('.pmin').val();
+    var newBreak = $('.bmin').val();
+
+    $('.pmin').val(newPomodoro);
+    $('.bmin').val(newBreak);
+
+    $('.min').text(pad(newPomodoro));
+    $('.setting').slideUp();
+
+  });
 
   $('.play img').click(function(){
     swapBtns();
-  })
-})
+  });
+});
